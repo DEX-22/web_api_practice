9aba08cc406a5cdba177d88231b74b39268a23b8
Dex <enriquesaenz0@gmail.com>
2023-12-02 17:34:55 -0500

    add motion

--- app.mjs
+++ app.mjs
@@ -1,38 +1,8 @@
-import { pre } from "./db/index.mjs";
+const d = DeviceMotionEventAcceleration
 
-export const ConnectionElement = document.getElementById('connection_section')
-const btnConnection = document.getElementById('btn-connection')
-
-const mostrarEstadoConexion = () => {
-    if (navigator.onLine) {
-        ConnectionElement.innerHTML += 'Conectado a internet \n';
-        ConnectionElement.innerHTML += `<br> Tipo de conexión: ${navigator.connection?.effectiveType}`;
-        ConnectionElement.innerHTML += `<br> Velocidad de descarga: ${navigator.connection?.downlink} Mbps`;
-        ConnectionElement.innerHTML += `<br> Velocidad de carga: ${navigator.connection?.uplink} Mbps`;
-        for (const item in Object.getOwnPropertyNames(navigator.connection)) {
-            console.log(item);
-        }
-        ConnectionElement.innerHTML +=`<br>`
-      
-
-    } else {
-        ConnectionElement.innerHTML += 'Sin conexión a internet \n';
-    }
-  };
-  btnConnection.addEventListener('click',()=>{
-    mostrarEstadoConexion()
-  })
-  // Escuchar eventos de conexión y desconexión
-  window.addEventListener('online', () => {
-    mostrarEstadoConexion();
-  });
-
-  window.addEventListener('offline', () => {
-    mostrarEstadoConexion();
-  }); 
+const motionContainer = document.getElementById('motion-container')
 
 
-
-
-
-
+motionContainer.innerHTML+= `<span> <b>X: </b>${d.x}</span>`
+motionContainer.innerHTML+= `<span> <b>Y: </b>${d.y}</span>`
+motionContainer.innerHTML+= `<span> <b>Z: </b>${d.z}</span>`
